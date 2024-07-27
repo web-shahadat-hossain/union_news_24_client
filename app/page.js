8e513c431a8575cbe9167f365cefb37c8c1224bb
web-shahadat-hossain <mdridoysikder862@gmail.com>
2024-07-27 15:31:01 +0600

    hedaer done

--- app/page.js
+++ app/page.js
@@ -1,7 +1,31 @@
+import HeadLines from "@/components/HeadLines";
+import SimpleNewsCard from "@/components/news/items/SimpleNewsCard";
+import LatestNews from "@/components/news/LatestNews";
+import Title from "@/components/Title";
+
 export default function Home() {
   return (
     <main>
-
+      <HeadLines />
+      <div className="bg-slate-100">
+        <div className="px-4 md:px-8 py-8">
+          <div className="flex flex-wrap">
+            <div className="w-full lg:w-6/12">
+              <LatestNews />
+            </div>
+            <div className="w-full lg:w-6/12 mt-5 lg:mt-0">
+              <div className="flex w-full flex-col gap-y-[14px] pl-0 lg:pl-2">
+                <Title title="Technology" />
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-[14px]">
+                  {[1, 2, 3, 4].map((item, i) => (
+                    <SimpleNewsCard item={item} key={i} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,17 +3,18 @@ import moment from "moment";
 import { FaFacebookF } from "react-icons/fa";
 import { AiFillYoutube, AiOutlineTwitter } from "react-icons/ai";
 import bg_header from "../assets/header-bg.jpg";
-import "moment/locale/bn"; // Import Bangla locale
+import logo from "../assets/logo.png";
+import adver_image from "../assets/sample-add.jpg";
+import Image from "next/image";
+import Header_Category from "./Header_Category";
 
 const Header = () => {
-  moment.locale("bn");
-
-  // Get current date and format it
-  const formattedDate = moment().format("LLLL");
   return (
     <div>
       <div className="px-5 lg:px-8 flex justify-between items-center bg-[#333333] text-[#cccccc]">
-        <span className="text-[13px] font-medium">{formattedDate}</span>
+        <span className="text-[13px] font-medium">
+          {moment().format("LLLL")}
+        </span>
         <div className="flex gap-x-[1px]">
           <a
             className="w-[37px] h-[35px] flex justify-center items-center bg-[#ffffff2b]"
@@ -36,12 +37,30 @@ const Header = () => {
         </div>
       </div>
       <div
-        className="w-full h-[240px]"
         style={{
           backgroundImage: `url(${bg_header.src})`,
           backgroundSize: "cover",
         }}
-      ></div>
+      >
+        <div className="px-8 py-14">
+          <div className="flex justify-center items-center flex-wrap">
+            <div className="md:w-4/12 w-full">
+              <div className="flex flex-col justify-center items-center md:items-start">
+                <Image className="w-[200px] h-[45px]" alt="logo" src={logo} />
+                <h2 className="text-[#cccccc] hidden md:block">
+                  union news 2024
+                </h2>
+              </div>
+            </div>
+            <div className="md:w-8/12 w-full mt-4 md:mt-0">
+              <div className="w-full flex justify-end">
+                <Image src={adver_image} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Header_Category />
     </div>
   );
 };
